2ee8e5e8e22cec8bd96c1ad6e4139462d2ef4c15
Richard Rohrig <richard.t.rohrig@gmail.com>
2022-02-14 22:59:55 +0100

    feature(views) auto register views in resources dir

--- src/ViewProvider.php
+++ src/ViewProvider.php
@@ -9,50 +9,56 @@ use Illuminate\View\ViewServiceProvider as ServiceProvider;
 
 class ViewProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->registerFactory();
-        $this->registerViewFinder();
-        $this->registerBladeCompiler();
-        $this->registerEngineResolver();
+  public function register()
+  {
+    $this->registerFactory();
+    $this->registerViewFinder();
+    $this->registerBladeCompiler();
+    $this->registerEngineResolver();
+  }
+
+  public function registerViewFinder()
+  {
+    $domainMakerViews =  $this->getDomainViewPaths();
+
+    $this->app->bind('view.finder', function ($app) use ($domainMakerViews) {
+
+      $paths = $app['config']['view.paths'];
+
+      foreach ($domainMakerViews as $path) {
+        $paths[] = $path;
+      }
+
+      return new FileViewFinder($app['files'], $paths);
+    });
+  }
+
+  public function getDomainViewPaths()
+  {
+    $viewPaths = [];
+    if (!file_exists(base_path('app/Domains'))) {
+      return;
+    };
+
+    $domains = array_diff(scandir(base_path('app/Domains')), array('.', '..'));
+
+    foreach ($domains as $domain) {
+      $dirs = array_diff(scandir(base_path('app/Domains/' . $domain)), array('.', '..'));
+      $resourcesDirExists = in_array('resources', $dirs);
+      if (!$resourcesDirExists) {
+        continue;
+      }
+
+      $dirs = array_diff(scandir(base_path('app/Domains/' . $domain . '/resources')), array('.', '..'));
+      $routesDirExists = in_array('views', $dirs);
+
+      if (!$routesDirExists) {
+        continue;
+      }
+
+      $viewPaths[] = base_path('app/Domains/' . $domain . '/resources/views');
     }
 
-    public function registerViewFinder()
-    {
-        $domainMakerViews =  $this->getDomainViewPaths();
-
-        $this->app->bind('view.finder', function ($app) use ($domainMakerViews) {
-
-            $paths = $app['config']['view.paths'];
-
-            foreach ($domainMakerViews as $path) {
-                $paths[] = $path;
-            }
-
-            return new FileViewFinder($app['files'], $paths);
-        });
-    }
-
-    public function getDomainViewPaths()
-    {
-        $viewPaths = [];
-        if (!file_exists(base_path('app/Domains'))) {
-            return;
-        };
-
-        $domains = array_diff(scandir(base_path('app/Domains')), array('.', '..'));
-
-        foreach ($domains as $domain) {
-            $dirs = array_diff(scandir(base_path('app/Domains/' . $domain)), array('.', '..'));
-            $routesDirExists = in_array('resources/views', $dirs);
-            if (!$routesDirExists) {
-                Log::info("$domain Domain missing views directory. Can't register views");
-                continue;
-            }
-
-            $viewPaths[] = base_path('app/Domains/' . $domain . '/resources/views');
-        }
-
-        return $viewPaths;
-    }
+    return $viewPaths;
+  }
 }
